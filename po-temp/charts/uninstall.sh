53d83b2094f133e880e10f3981c6bd65743fcf00
José Eduardo Gutiérrez Conejo <jose.gutierrez.conejo@gmail.com>
2025-03-05 10:36:13 -0600

    fix: naming

--- po-temp/charts/uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-helm uninstall public
-sleep 20
-helm uninstall private
-sleep 10
-helm uninstall namespace
--- po-temp/docker/asterisk/app/script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Start Asterisk in verbose mode
-asterisk -cvvvvv
