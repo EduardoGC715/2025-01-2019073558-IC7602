727bd6cf87a562bd5a753ada84b5894b11f964ae
Daniel Granados <dandiegogranados@yahoo.com>
2025-03-06 19:30:57 -0600

    Fix Asterisk Cluster IP

--- po/docker/router/app/script.sh
+++ po/docker/router/app/script.sh
@@ -9,47 +9,36 @@ INGRESS=$(nslookup $INGRESSURL | awk '/^Address: / { print $2 }')
 iptables -A INPUT -j ACCEPT
 iptables -A OUTPUT -j ACCEPT
 
-# Log and Accept Forwarding Rules
-# iptables -A FORWARD -i eth0 -o eth0 -p tcp --dport 8080 -j LOG --log-prefix "FORWARD APACHE1: " --log-level 4
+# Accept Forwarding Rules
+iptables -A FORWARD -i eth0 -o eth0 -p tcp --dport 8080 -j ACCEPT
 iptables -A FORWARD -i eth0 -o eth0 -p tcp --dport 8080 -j ACCEPT
 
-# iptables -A FORWARD -p udp --dport 5601 -j LOG --log-prefix "FORWARD ASTERISK UDP: " --log-level 4
-iptables -A FORWARD -p udp --dport 5601 -j ACCEPT
 
-# iptables -A FORWARD -p tcp --dport 5601 -j LOG --log-prefix "FORWARD ASTERISK TCP: " --log-level 4
-iptables -A FORWARD -p tcp --dport 5601 -j ACCEPT
+iptables -A FORWARD -i eth0 -o eth0 -p udp --dport 5060 -j ACCEPT
+iptables -A FORWARD -i eth0 -o eth0 -p tcp --dport 5060 -j ACCEPT
+
+iptables -A FORWARD -i eth0 -o eth0 -p tcp --dport 80 -j ACCEPT
 
 # Allow inbound RTP traffic to Asterisk
-iptables -A FORWARD -p udp --dport 10000:10010 -j ACCEPT
-iptables -A FORWARD -p udp --sport 10000:10010 -j ACCEPT
+# iptables -A FORWARD -p udp --dport 10000:10010 -j ACCEPT
+# iptables -A FORWARD -p udp --sport 10000:10010 -j ACCEPT
 
 # Forward RTP packets to Asterisk
 iptables -t nat -A PREROUTING -i eth0 -p udp --dport 10000:10010 -j DNAT --to-destination $ASTERISK
 
 
-iptables -A FORWARD -i eth0 -o eth0 -p tcp --dport 80 -j ACCEPT
-
 # Log and Apply DNAT (Destination NAT) for Apache1
-# iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 8080 -j LOG --log-prefix "DNAT APACHE1: " --log-level 4
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 8080 -j DNAT --to-destination $APACHE1:80
-# iptables -t nat -A OUTPUT -p tcp --dport 8080 -j LOG --log-prefix "DNAT OUTPUT APACHE1: " --log-level 4
 iptables -t nat -A OUTPUT -p tcp --dport 8080 -j DNAT --to-destination $APACHE1:80
 
 # Log and Apply DNAT for Apache2
-# iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 8081 -j LOG --log-prefix "DNAT APACHE2: " --log-level 4
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 8081 -j DNAT --to-destination $APACHE2:80
-# iptables -t nat -A OUTPUT -p tcp --dport 8081 -j LOG --log-prefix "DNAT OUTPUT APACHE2: " --log-level 4
 iptables -t nat -A OUTPUT -p tcp --dport 8081 -j DNAT --to-destination $APACHE2:80
 
 # Log and Apply DNAT for Asterisk (UDP and TCP)
-# iptables -t nat -A PREROUTING -i eth0 -p udp --dport 5601 -j LOG --log-prefix "DNAT ASTERISK UDP: " --log-level 4
 iptables -t nat -A PREROUTING -i eth0 -p udp --dport 5601 -j DNAT --to-destination $ASTERISK:5060
-# iptables -t nat -A OUTPUT -p udp --dport 5601 -j LOG --log-prefix "DNAT OUTPUT ASTERISK UDP: " --log-level 4
 iptables -t nat -A OUTPUT -p udp --dport 5601 -j DNAT --to-destination $ASTERISK:5060
-
-# iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 5601 -j LOG --log-prefix "DNAT ASTERISK TCP: " --log-level 4
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 5601 -j DNAT --to-destination $ASTERISK:5060
-# iptables -t nat -A OUTPUT -p tcp --dport 5601 -j LOG --log-prefix "DNAT OUTPUT ASTERISK TCP: " --log-level 4
 iptables -t nat -A OUTPUT -p tcp --dport 5601 -j DNAT --to-destination $ASTERISK:5060
 
 # DNAT rule for ingress controller
@@ -61,3 +50,15 @@ iptables -t nat -A POSTROUTING -j MASQUERADE
 
 # Keep container running
 tail -f /dev/null
+
+# iptables -A FORWARD -i eth0 -o eth0 -p tcp --dport 8080 -j LOG --log-prefix "FORWARD APACHE1: " --log-level 4
+# iptables -A FORWARD -p udp --dport 5601 -j LOG --log-prefix "FORWARD ASTERISK UDP: " --log-level 4
+# iptables -A FORWARD -p tcp --dport 5601 -j LOG --log-prefix "FORWARD ASTERISK TCP: " --log-level 4
+# iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 8080 -j LOG --log-prefix "DNAT APACHE1: " --log-level 4
+# iptables -t nat -A OUTPUT -p tcp --dport 8080 -j LOG --log-prefix "DNAT OUTPUT APACHE1: " --log-level 4
+# iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 8081 -j LOG --log-prefix "DNAT APACHE2: " --log-level 4
+# iptables -t nat -A OUTPUT -p tcp --dport 8081 -j LOG --log-prefix "DNAT OUTPUT APACHE2: " --log-level 4
+# iptables -t nat -A PREROUTING -i eth0 -p udp --dport 5601 -j LOG --log-prefix "DNAT ASTERISK UDP: " --log-level 4
+# iptables -t nat -A OUTPUT -p udp --dport 5601 -j LOG --log-prefix "DNAT OUTPUT ASTERISK UDP: " --log-level 4
+# iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 5601 -j LOG --log-prefix "DNAT ASTERISK TCP: " --log-level 4
+# iptables -t nat -A OUTPUT -p tcp --dport 5601 -j LOG --log-prefix "DNAT OUTPUT ASTERISK TCP: " --log-level 4
