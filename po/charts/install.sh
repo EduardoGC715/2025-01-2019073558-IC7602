0e9d74e9cab44834133580752a4673522bfd51f1
Diegomm146 <dmm1462003@gmail.com>
2025-03-11 23:46:56 -0600

    automated wsl ip

--- po/charts/install.sh
+++ po/charts/install.sh
@@ -5,11 +5,15 @@ set -e  # Exit on error to prevent partial installs
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "Running script from: $SCRIPT_DIR"
 
+# Get the WSL IP address
+EXTERNAL_IP=$(ipconfig | grep -A 5 "WSL" | grep "IPv4 Address" | awk '{print $NF}')
+echo "WSL IP: $EXTERNAL_IP"
+
 # Install or upgrade the namespace chart
 helm upgrade --install namespace "$SCRIPT_DIR/namespace"
 sleep 10
 
-export $(grep -v '^#' .env | xargs)  # Load .env variables
+#export $(grep -v '^#' .env | xargs)
 # Install or upgrade the private chart
 helm upgrade --install private "$SCRIPT_DIR/private" --set config.asterisk.external_ip=$EXTERNAL_IP
 sleep 20
